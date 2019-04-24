371742f3d013969f783d91f4fd1088d3ad117995
Mauricio Vásquez Chalup <mauriciovch98@gmail.com>
2019-04-24 12:33:00 -0700

    commit con MVCpractico

---
+++ practico/MVCpractico/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCpractico.MvcApplication" Language="C#" %>
---
+++ practico/MVCpractico/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCpractico.Startup))]
+namespace MVCpractico
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
