13f1b72fc5a315ae288f7e928e258f51aa507663
Mauricio Vásquez Chalup <mauriciovch98@gmail.com>
2019-04-24 12:55:49 -0700

    DataContext y Controller

---
+++ practico/MVCpractico/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCpractico.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCpractico.Models.Producto> Productoes { get; set; }
+    }
+}
